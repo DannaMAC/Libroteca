da9b6e4a9935edc4bb20afa8c41ac55d74b3df3f
DannaMAC <116323415+DannaMAC@users.noreply.github.com>
2024-05-27 19:32:48 -0600

    Tablas
    Se agregaron las tablas de la base de datos en el html

--- script.js
+++ script.js
@@ -1,3 +1,144 @@
+var adminUsername = "admin"; // Nombre de usuario del administrador
+var adminPassword = "admin10"; // Contraseña del administrador
+
+function authenticate(event) {
+    event.preventDefault(); 
+
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+
+    if (username === adminUsername && password === adminPassword) {
+        console.log("Inicio de sesión exitoso para el administrador");
+        document.getElementById('loginPage').style.display = 'none';
+        document.getElementById('mainPage').style.display = 'block';
+    } else {
+        alert('Usuario o contraseña incorrectos');
+    }
+}
+
+function showSection(sectionId) {
+    document.querySelectorAll('main section').forEach(section => {
+        section.style.display = 'none';
+    });
+
+    document.getElementById(sectionId).style.display = 'block';
+}
+
+function logout() {
+    document.getElementById('loginPage').style.display = 'flex'; 
+    document.getElementById('mainPage').style.display = 'none';
+}
+
+function registerUser(event) {
+    event.preventDefault();
+
+    var newFirstName = document.getElementById('newFirstName').value;
+    var newLastName = document.getElementById('newLastName').value;
+    var newAddress = document.getElementById('newAddress').value;
+    var newPhoneNumber = document.getElementById('newPhoneNumber').value;
+    var newEmail = document.getElementById('newEmail').value;
+
+    console.log('Usuario registrado:', {
+        firstName: newFirstName,
+        lastName: newLastName,
+        address: newAddress,
+        phoneNumber: newPhoneNumber,
+        email: newEmail
+    });
+
+    // Aquí puedes realizar una llamada AJAX para insertar los datos en la base de datos
+    // Por ejemplo, utilizando Fetch API o XMLHttpRequest
+
+    alert('Usuario registrado con éxito');
+    document.getElementById('userRegistrationForm').reset();
+}
+
+function addBook(event) {
+    event.preventDefault();
+
+    var bookName = document.getElementById('bookName').value;
+    var bookAuthor = document.getElementById('bookAuthor').value;
+    var bookDescription = document.getElementById('bookDescription').value;
+    var bookEdition = document.getElementById('bookEdition').value;
+    var bookEditor = document.getElementById('bookEditor').value;
+
+    console.log('Libro agregado:', {
+        name: bookName,
+        author: bookAuthor,
+        description: bookDescription,
+        edition: bookEdition,
+        editor: bookEditor
+    });
+
+    // Aquí puedes realizar una llamada AJAX para insertar los datos en la base de datos
+    // Por ejemplo, utilizando Fetch API o XMLHttpRequest
+
+    alert('Libro agregado con éxito');
+    document.getElementById('bookForm').reset();
+}
+
+function createLoan(event) {
+  event.preventDefault();
+
+  var clientId = document.getElementById('clientId').value;
+  var librarianId = document.getElementById('librarianId').value;
+  var stockId = document.getElementById('stockId').value;
+  var loanStartDate = document.getElementById('loanStartDate').value;
+  var loanDueDate = document.getElementById('loanDueDate').value;
+  var loanReturnDate = document.getElementById('loanReturnDate').value;
+  var status = document.getElementById('status').value;
+
+  console.log('Préstamo creado:', {
+      clientId: clientId,
+      librarianId: librarianId,
+      stockId: stockId,
+      loanStartDate: loanStartDate,
+      loanDueDate: loanDueDate,
+      loanReturnDate: loanReturnDate,
+      status: status
+  });
+
+  alert('Préstamo creado con éxito');
+  document.getElementById('loanForm').reset();
+}
+function createStock(event) {
+  event.preventDefault();
+
+  var stockId = document.getElementById('stockId').value;
+  var status = document.getElementById('status').value;
+  var bookId = document.getElementById('bookId').value;
+  var available = document.getElementById('available').value;
+
+  console.log('Stock agregado:', {
+      stockId: stockId,
+      status: status,
+      bookId: bookId,
+      available: available
+  });
+
+  alert('Stock agregado con éxito');
+  document.getElementById('stockForm').reset();
+}
+function createFine(event) {
+  event.preventDefault();
+
+  var fineId = document.getElementById('fineId').value;
+  var amountToPay = document.getElementById('amountToPay').value;
+  var fineDate = document.getElementById('fineDate').value;
+  var status = document.getElementById('status').value;
+  var loanId = document.getElementById('loanId').value;
+
+  console.log('Fine agregado:', {
+      fineId: fineId,
+      amountToPay: amountToPay,
+      fineDate: fineDate,
+      status: status,
+      loanId: loanId
+  });
+
+  alert('Fine agregado con éxito');
+  document.getElementById('fineForm').reset();
+}
 /*const oracledb = require('oracledb');
 async function runApp()
 {
